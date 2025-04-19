766ae184702e7daeddba6690401eab80b282d904
KchrisSpace <chris514863@gmail.com>
2025-04-20 02:10:38 +0800

    1.0

--- src/views/introduction/Introduction.vue
+++ src/views/introduction/Introduction.vue
@@ -1,31 +1,26 @@
 <!--  简介-->
 <template>
-    <div class="introduction" >
-        <div class="bg"></div>
-
-    </div>
+  <div class="introduction">
+    <div class="bg"></div>
+  </div>
 </template>
 
-<script setup >
+<script setup></script>
 
-</script>
-
-<style  scoped>
-.introduction{
-    width: 100vw;
-    height: 100vh;
-     
+<style scoped>
+.introduction {
+  width: 100vw;
+  height: 100vh;
 }
-.bg{
-    width: inherit;
-    height: inherit;
-    background-image: url(/public/png/bg-girl.png);
-    /* background-position:  0px; */
-    background-size: contain;
-    background-size: cover;
-    background-repeat: no-repeat;
-    position: fixed;
-    z-index: -1;
-    
+.bg {
+  width: inherit;
+  height: inherit;
+  background-image: url(/public/png/bg-girl.png);
+  /* background-position:  0px; */
+  background-size: contain;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: fixed;
+  z-index: -1;
 }
 </style>
